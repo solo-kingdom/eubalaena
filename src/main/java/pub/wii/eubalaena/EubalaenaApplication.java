d8378b29a53e7b613cc92b23023dfa473c89fee6
wii <zhenkai.sun@qq.com>
2021-05-07 08:38:08 +0800

    first commit

---
+++ src/main/java/pub/wii/eubalaena/EubalaenaApplication.java
@@ -0,0 +1,13 @@
+package pub.wii.eubalaena;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EubalaenaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EubalaenaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pub/wii/eubalaena/EubalaenaApplicationTests.java
@@ -0,0 +1,13 @@
+package pub.wii.eubalaena;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EubalaenaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
